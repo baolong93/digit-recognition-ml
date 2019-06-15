a6e814e860defcc888d8f56ab648db4d40587d43
unknown <kolpak.conrad@gmail.com>
2019-06-15 20:16:53 +0100

    first commit, initial canvas drawn

---
+++ public/main.js
@@ -0,0 +1,62 @@
+window.addEventListener('load', () => {
+
+  const canvas = new fabric.Canvas('canvas');
+  canvas.isDrawingMode= true
+  let width = 1200, height = 850
+
+  canvas.setHeight(height);
+  canvas.setWidth(width);
+  canvas.backgroundColor="black";
+
+  let pencil = new fabric.PencilBrush(canvas)
+  pencil.width = 10
+  pencil.color = 'white'
+
+  canvas.freeDrawingBrush = pencil
+
+
+  const guessButton = window.guess
+
+  guessButton.addEventListener('click', () => {
+
+    exportCanvas(canvas).then((res) => {
+      console.log(res)
+      postData('/makeGuess', {img:res}).then((response) => {
+        console.log(response)
+      })
+    })
+  })
+
+
+
+})
+
+
+function exportCanvas(canvas) {
+  return new Promise((resolve) => {
+    // let scaleRatio = Math.min(28/canvas.getWidth(), 28/canvas.getHeight());
+    // canvas.setDimensions({ width: canvas.getWidth() * scaleRatio, height: canvas.getHeight() * scaleRatio })
+    // canvas.setZoom(scaleRatio)
+    const exportedImage = canvas.toDataURL({
+      format: 'png',
+    });
+
+    resolve(exportedImage)
+  })
+}
+
+function postData(url, data) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'POST',
+      headers: new Headers({'content-type': 'application/json'}),
+      body: JSON.stringify(data),
+    }).then(function (res) {
+      return res.json()
+    }).then((data) => {
+      resolve(data)
+    }).catch(error => {
+      reject(error)
+    });
+  })
+}
